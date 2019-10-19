b1cce744266c4f50e21175c2bcc6e797f9105426
aneagoie <aneagoie1@gmail.com>
2019-10-19 10:46:28 -0400

    Updates

---
+++ precache-manifest.e4b6a4bd52a3dfb0db4edd63e68f0a85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e561ec5622c87cc2371aa614e2f1d2",
+    "url": "/Center-For-Robotos-Who-Cant-Be-In-The-App-Store-And-Wanna-Learn-To-Do-Other-Stuff-Good-Too-update/index.html"
+  },
+  {
+    "revision": "29590987f10094051ea3",
+    "url": "/Center-For-Robotos-Who-Cant-Be-In-The-App-Store-And-Wanna-Learn-To-Do-Other-Stuff-Good-Too-update/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "01e3a403115739c14a8a",
+    "url": "/Center-For-Robotos-Who-Cant-Be-In-The-App-Store-And-Wanna-Learn-To-Do-Other-Stuff-Good-Too-update/static/css/main.0309cf25.chunk.css"
+  },
+  {
+    "revision": "29590987f10094051ea3",
+    "url": "/Center-For-Robotos-Who-Cant-Be-In-The-App-Store-And-Wanna-Learn-To-Do-Other-Stuff-Good-Too-update/static/js/2.b72d6b25.chunk.js"
+  },
+  {
+    "revision": "01e3a403115739c14a8a",
+    "url": "/Center-For-Robotos-Who-Cant-Be-In-The-App-Store-And-Wanna-Learn-To-Do-Other-Stuff-Good-Too-update/static/js/main.106103c8.chunk.js"
+  },
+  {
+    "revision": "b225386d38edf5fcc9b2",
+    "url": "/Center-For-Robotos-Who-Cant-Be-In-The-App-Store-And-Wanna-Learn-To-Do-Other-Stuff-Good-Too-update/static/js/runtime-main.6c8a3359.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Center-For-Robotos-Who-Cant-Be-In-The-App-Store-And-Wanna-Learn-To-Do-Other-Stuff-Good-Too-update/static/media/SEGA.12a3db8b.woff"
+  }
+]);
